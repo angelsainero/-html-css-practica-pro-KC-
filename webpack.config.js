21a8879764c9f115cb5af9b1d8d7d68fa01d6704
kevinccbsg <kevinccbsg@gmail.com>
2023-03-20 20:19:41 +0100

    feat: add naming to the output bundle

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   mode: 'development',
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
