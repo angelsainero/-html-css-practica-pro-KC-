e9911ff9ca558e64eb48e2021d7ecec1487163f4
kevinccbsg <kevinccbsg@gmail.com>
2023-03-20 23:33:17 +0100

    feat: add prod mode

--- webpack.common.js
+++ webpack.common.js
@@ -6,8 +6,6 @@ module.exports = {
     home: './src/home.js',
     projects: './src/projectsPage.js',
   },
-  mode: 'development',
-  devtool: 'inline-source-map', // 'source-map'
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
---
+++ webpack.prod.js
@@ -0,0 +1,30 @@
+const { merge } = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+    ],
+  },
+});
