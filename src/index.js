7a0edbc640b7096a75ad15903e17345b9f5e5850
kevinccbsg <kevinccbsg@gmail.com>
2023-03-20 19:59:35 +0100

    feat: add webpack mode and lodash example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // import _ from 'lodash';
-// import join from 'lodash/join';
+import join from 'lodash/join';
 
 const hello = 'hello';
 const world = 'world';
 
-console.log(`${hello} ${world}`);
-// console.log(join([hello, world], ' '));
+// console.log(`${hello} ${world}`);
+console.log(join([hello, world], ' '));
