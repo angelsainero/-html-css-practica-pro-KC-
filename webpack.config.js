e32766aead1046d8dd5f85e36a7fae80a2b406e2
kevinccbsg <kevinccbsg@gmail.com>
2023-03-20 20:11:46 +0100

    feat: add output to the config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
+const path = require('path');
+
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
 };
