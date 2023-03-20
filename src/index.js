1c530f74414a20c8be8d7f1a4bbff0420856fdb4
kevinccbsg <kevinccbsg@gmail.com>
2023-03-20 19:55:20 +0100

    feat: add entry point

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import _ from 'lodash';
+// import join from 'lodash/join';
+
+const hello = 'hello';
+const world = 'world';
+
+console.log(`${hello} ${world}`);
+// console.log(join([hello, world], ' '));
