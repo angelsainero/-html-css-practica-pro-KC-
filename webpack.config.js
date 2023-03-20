c5b22774a9b8bd926e326efe84efba6e28e3c119
kevinccbsg <kevinccbsg@gmail.com>
2023-03-20 21:56:30 +0100

    feat: add css loader and style loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,4 +12,12 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+    ]
+  },
 };
